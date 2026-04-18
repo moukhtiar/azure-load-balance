73c6a3999e982bd6fb0f2b68296fdb8af4a52e09
Muktar Mohamed <criora@yahoo.com>
2026-04-18 20:20:35 +0400

    Add Azure resources for networking and VMs

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,155 @@
 
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = local.vnet_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = var.vnet_address_space
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = local.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet_address_prefix
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = local.nsg_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "allow-http"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_public_ip" "lb_pip" {
+  name                = local.public_ip_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_lb" "lb" {
+  name                = local.lb_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = local.fe_name
+    public_ip_address_id = azurerm_public_ip.lb_pip.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "backend" {
+  name            = local.be_pool_name
+  loadbalancer_id = azurerm_lb.lb.id
+}
+
+resource "azurerm_lb_probe" "http_probe" {
+  name            = local.probe_name
+  loadbalancer_id = azurerm_lb.lb.id
+  protocol        = "Http"
+  port            = 80
+  request_path    = "/"
+}
+
+resource "azurerm_lb_rule" "http_rule" {
+  name                           = local.rule_name
+  loadbalancer_id                = azurerm_lb.lb.id
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = local.fe_name
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.backend.id]
+  probe_id                       = azurerm_lb_probe.http_probe.id
+}
+
+resource "azurerm_network_interface" "nic" {
+  for_each            = local.vms
+  name                = "nic-${each.value.name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = each.value.private_ip
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  for_each                  = local.vms
+  network_interface_id      = azurerm_network_interface.nic[each.key].id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "lb_assoc" {
+  for_each                = local.vms
+  network_interface_id    = azurerm_network_interface.nic[each.key].id
+  ip_configuration_name   = "ipconfig1"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.backend.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  for_each                        = local.vms
+  name                            = each.value.name
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = var.vm_size
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
+  disable_password_authentication = false
+  network_interface_ids           = [azurerm_network_interface.nic[each.key].id]
+
+  os_disk {
+    name                 = "osdisk-${each.value.name}"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  computer_name = each.value.name
+}
+
+resource "azurerm_virtual_machine_extension" "nginx" {
+  for_each                   = local.vms
+  name                       = "install-nginx"
+  virtual_machine_id         = azurerm_linux_virtual_machine.vm[each.key].id
+  publisher                  = "Microsoft.Azure.Extensions"
+  type                       = "CustomScript"
+  type_handler_version       = "2.1"
+  auto_upgrade_minor_version = true
+
+  settings = <<SETTINGS
+{
+  "commandToExecute": "bash -c 'apt-get update && apt-get install -y nginx && systemctl enable nginx && systemctl start nginx && HOSTNAME=$(hostname) && echo \"<!DOCTYPE html><html><head><title>${each.value.name}</title></head><body style=\\\"font-family:Arial;text-align:center;margin-top:60px;\\\"><h1>${each.value.page_title}</h1><p>Hostname: $HOSTNAME</p><p>Served by NGINX on Azure VM</p></body></html>\" > /var/www/html/index.html'"
+}
+SETTINGS
+}
+
+output "load_balancer_public_ip" {
+  value = azurerm_public_ip.lb_pip.ip_address
+}
